cdab2b7d94549e013b4d3da36491c2f1e1ed1015
ShikenNuggets <carter.rennick@gmail.com>
2026-06-19 18:02:26 -0400

    Initial project setup

---
+++ ArkhamSaveParser.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace ArkhamSaveParser.Tests
+{
+	[TestClass]
+	public sealed class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var test = true;
+			Assert.IsTrue(test); // Sanity check
+		}
+	}
+}
---
+++ ArkhamSaveParser/CompressedSave.cs
@@ -0,0 +1,7 @@
+﻿namespace ArkhamSaveParser
+{
+	public class CompressedSave
+	{
+
+	}
+}
