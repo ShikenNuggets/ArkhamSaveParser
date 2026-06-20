8073457654ba57077c916bff89bf5b4cc166887b
ShikenNuggets <carter.rennick@gmail.com>
2026-06-19 21:46:24 -0400

    Save decompressor

---
+++ ArkhamSaveParser.Tests/DecompressedSaveTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArkhamSaveParser.Tests {
+	[TestClass]
+	public sealed class DecompressedSaveTest {
+		[TestMethod]
+		public void DecompressSave_City_TestSave0() {
+			var compressedSave = new CompressedSave("TestData/City/TestSave0.sgd");
+			var decompressedSave = new DecompressedSave(compressedSave);
+
+			Assert.AreEqual("33625eba87c4d4251ff9d1d765beb45af477e00ff1253b76b7d22029d6b1c6c0", decompressedSave.GetHashString());
+		}
+	}
+}
---
+++ ArkhamSaveParser/DecompressedSave.cs
@@ -0,0 +1,47 @@
+﻿using lzo.net;
+
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace ArkhamSaveParser {
+	public class DecompressedSave {
+		public DecompressedSave(CompressedSave compressedSave) {
+			if (compressedSave.TotalDecompressedSize <= 0) {
+				throw new InvalidOperationException("Invalid total decompressed size");
+			}
+
+			rawData = new byte[compressedSave.TotalDecompressedSize];
+
+			int destOffset = 0;
+			foreach (var segment in compressedSave.Segments) {
+				var compressedBuffer = compressedSave.ReadFileSegment(segment);
+				var compressedMs = new MemoryStream(compressedBuffer);
+				var lzo = new LzoStream(compressedMs, CompressionMode.Decompress);
+				int totalRead = 0;
+				while (totalRead < segment.decompressedSize) {
+					int read = lzo.Read(rawData, destOffset + totalRead, segment.decompressedSize - totalRead);
+					if (read <= 0) {
+						break;
+					}
+
+					totalRead += read;
+				}
+
+				if (totalRead != segment.decompressedSize) {
+					destOffset += totalRead;
+					Console.WriteLine("Warning: totalRead was " + totalRead + ", expected " + segment.decompressedSize);
+					continue;
+				}
+
+				destOffset += segment.decompressedSize;
+			}
+		}
+
+		public string GetHashString() {
+			var totalHash = SHA256.HashData(rawData);
+			return Convert.ToHexStringLower(totalHash);
+		}
+
+		private byte[] rawData;
+	}
+}
