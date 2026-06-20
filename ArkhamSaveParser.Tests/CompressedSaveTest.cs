97085fa2c950915ee956d9525a69a5230ae94b46
ShikenNuggets <carter.rennick@gmail.com>
2026-06-19 21:38:47 -0400

    Compressed save loader

---
+++ ArkhamSaveParser.Tests/CompressedSaveTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArkhamSaveParser.Tests {
+	[TestClass]
+	public sealed class CompressedSaveTest {
+		[TestMethod]
+		public void LoadCompressedSave_City_TestSave0()
+		{
+			var save = new CompressedSave("TestData/City/TestSave0.sgd");
+			Assert.AreEqual(3, save.Segments.Count);
+
+			Assert.AreEqual(152, save.Segments[0].compressedSize);
+			Assert.AreEqual(357, save.Segments[0].decompressedSize);
+			Assert.AreEqual(64, save.Segments[0].offset);
+
+			Assert.AreEqual(1447, save.Segments[1].compressedSize);
+			Assert.AreEqual(6352, save.Segments[1].decompressedSize);
+			Assert.AreEqual(240, save.Segments[1].offset);
+
+			Assert.AreEqual(11558, save.Segments[2].compressedSize);
+			Assert.AreEqual(30791, save.Segments[2].decompressedSize);
+			Assert.AreEqual(1711, save.Segments[2].offset);
+
+			Assert.AreEqual(37500, save.TotalDecompressedSize);
+		}
+	}
+}
--- ArkhamSaveParser/CompressedSave.cs
+++ ArkhamSaveParser/CompressedSave.cs
@@ -1,7 +1,94 @@
-﻿namespace ArkhamSaveParser
-{
-	public class CompressedSave
-	{
+﻿using System.Buffers.Binary;
 
+namespace ArkhamSaveParser {
+	public struct FileSegment(int inOffset, int inCompressedSize, int inDecompressedSize) {
+		public long offset = inOffset;
+		public int compressedSize = inCompressedSize;
+		public int decompressedSize = inDecompressedSize;
+	}
+
+	public class CompressedSave {
+		public CompressedSave(string filePath) {
+			rawData = File.ReadAllBytes(filePath);
+			offsets = ExtractSaveOffsets(rawData);
+
+			reader = new BinaryReader(new MemoryStream(rawData));
+
+			segments = [];
+			FindSegments();
+
+			if (totalDecompressedSize <= 0) {
+				throw new InvalidOperationException("Invalid total decompressed size");
+			}
+		}
+
+		public byte[] ReadFileSegment(FileSegment segment) {
+			reader.BaseStream.Seek(segment.offset, SeekOrigin.Begin);
+			var compressedBuffer = new byte[segment.compressedSize];
+			reader.Read(compressedBuffer, 0, segment.compressedSize);
+			return compressedBuffer;
+		}
+
+		public List<FileSegment> Segments { get { return segments; } }
+		public int TotalDecompressedSize { get { return totalDecompressedSize; } }
+
+		private byte[] rawData;
+		private int[] offsets;
+		private List<FileSegment> segments;
+		private int totalDecompressedSize;
+		private BinaryReader reader;
+
+		private void FindSegments() {
+			totalDecompressedSize = 0;
+			foreach (var offset in offsets) {
+				FileSegment segment;
+
+				reader.BaseStream.Seek(offset, SeekOrigin.Begin);
+				//- Header: 0x9e2a83c1 (2653586369)
+				_ = Get32(reader); // header
+				_ = Get32(reader); // + 4, unknown
+				segment.compressedSize = (int)Get32(reader); // + 8
+				segment.decompressedSize = (int)Get32(reader); // + 12
+				reader.BaseStream.Position += 8; // skip 8 bytes before compressed data
+				segment.offset = reader.BaseStream.Position;
+				segments.Add(segment);
+
+				totalDecompressedSize += segment.decompressedSize;
+			}
+
+			if (totalDecompressedSize >= int.MaxValue) {
+				totalDecompressedSize = -1;
+			}
+		}
+
+		private static int[] ExtractSaveOffsets(byte[] buffer) {
+			var offsets = new List<int>();
+
+			for (int i = 0; i < buffer.Length - 4; i++) {
+				if (buffer[i] != 0x9E) {
+					continue;
+				}
+
+				if (buffer[i + 1] != 0x2A) {
+					continue;
+				}
+
+				if (buffer[i + 2] != 0x83) {
+					continue;
+				}
+
+				if (buffer[i + 3] != 0xC1) {
+					continue;
+				}
+
+				offsets.Add(i);
+			}
+
+			return offsets.ToArray();
+		}
+
+		private static uint Get32(BinaryReader reader) {
+			return BinaryPrimitives.ReverseEndianness(reader.ReadUInt32());
+		}
 	}
 }
