b1e1f60ca5740f3ddb2df27c2e658c1102e13b9e
ShikenNuggets <carter.rennick@gmail.com>
2026-06-20 13:40:34 -0400

    Added another save file for testing

--- ArkhamSaveParser.Tests/DecompressedSaveTest.cs
+++ ArkhamSaveParser.Tests/DecompressedSaveTest.cs
@@ -12,5 +12,13 @@ namespace ArkhamSaveParser.Tests {
 
 			Assert.AreEqual("33625eba87c4d4251ff9d1d765beb45af477e00ff1253b76b7d22029d6b1c6c0", decompressedSave.GetHashString());
 		}
+
+		[TestMethod]
+		public void DecompressSave_City_TestSave1() {
+			var compressedSave = new CompressedSave("TestData/City/TestSave1.sgd");
+			var decompressedSave = new DecompressedSave(compressedSave);
+
+			Assert.AreEqual("e4e2a37d34373e75d19fd8ce367eec7b3ab19ec634b0baaf0263aed702517646", decompressedSave.GetHashString());
+		}
 	}
 }
